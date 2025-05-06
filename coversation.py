31f00234754c9b497100dda3f5496451b4862076
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-05-06 17:43:57 +0000

    :3

---
+++ coversation.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ")
+
+print('Hello ' + name + '!')
+
+fun = input("what do you do for fun? ")
+
+print("I love " + fun + " as well. It is my favroite thing to do.")
+
+crime = input("would you like commit war crimes? ")
+
+if crime == "yes" or crime == "Yes":
+    print("Let's go")
+else:
+    print("Aw man")
