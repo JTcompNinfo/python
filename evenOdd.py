615893255bb04d0e0f1e74eae049658561e9d17e
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-05-08 17:51:02 +0000

    stuff

---
+++ evenOdd.py
@@ -0,0 +1,13 @@
+def check_even_odd():
+    # Get user input
+    num = int(input('Enter a whole number: '))
+
+    # Use if else to check if numer is even or odd
+
+    if num % 2 == 0:
+        print("The number is even.")
+    else:
+        print("Your number is odd.")
+
+# Call the function to run th program
+check_even_odd()
