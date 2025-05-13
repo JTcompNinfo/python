0df2d90e6895548004cae839847c0e7d475e81f5
JTcompNinfo <cte-joel.dinovo@sllboces.org>
2025-05-13 15:48:45 +0000

    klkl

--- diceRoller.py
+++ diceRoller.py
@@ -1,15 +1,33 @@
 import random
-# Loop
+#loop
 count = 0
 while (count == 0):
     print("///////////////////////////////////////////////////////")
-    # What sided die and how many
-    b = int(input("What sided dice do you want to roll?"))
+	#What sided die and how many
+    b = int(input("What sided dice do you want to roll? "))
     if b <= 1:
-        print("invalid number making it 2")
+        print("Invalid number making it 2")
         b = 2
-    i = int(input("How many dice would you like to roll?"))
-    #Genrate n random integers between a and b
+    i = int(input("How many dice do you want to roll? "))
+
+	# generate n random integers between a and b
     n = i
     a = 1
-    ran_nums = [random.randint(a, b) for _ in range(n)]
+    rand_nums = [random.randint(a, b) for _ in range(n)]
+ 
+	# calculate the sum of the random integers
+    rand_sum = sum(rand_nums)
+    
+	# print the results
+    print("Dice rolls:", rand_nums)
+    print("Sum of rolls:", rand_sum)
+    
+	#Crits
+    if rand_sum == n* a:
+    	print("Womp Womp, You crit fail :(")
+    if rand_sum == n* b:
+    	print("Yippe you crit :)")
+    
+    end = int(input("Do you want to end the program? \n1 Yes \n2) No \n"))
+    if end == 1:
+        count = 1
